065d9c4e02fe390b64bdfbbdeaf761e7e908b398
danieldenton <danielmdenton@gmail.com>
2023-01-09 13:45:35 -0800

    setup

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -5,4 +5,12 @@ import Congrats from "./Congrtats";
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
 test("renders without error", () => {});
+
+test("renders no text when success is false", () => {});
+
+test("renders non-empty congrats message when success is true", () => {});
