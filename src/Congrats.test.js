4af4cc6ddceae8db59704c33faa89988432f606a
danieldenton <danielmdenton@gmail.com>
2023-01-09 14:05:24 -0800

    fix component name

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,7 +1,7 @@
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "@cfaester/enzyme-adapter-react-18";
-
-import Congrats from "./Congrtats";
+import { findByTestAttr } from "./test/test.utils";
+import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -9,8 +9,20 @@ const setup = (props = {}) => {
   return shallow(<Congrats {...props} />);
 };
 
-test("renders without error", () => {});
+test("renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.length).toBe.apply(1);
+});
 
-test("renders no text when success is false", () => {});
+test("renders no text when success is false", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.text()).toBe("");
+});
 
-test("renders non-empty congrats message when success is true", () => {});
+test("renders non-empty congrats message when success is true", () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+  expect(message.text().length).not.toBe(0);
+});
