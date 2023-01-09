e73c64369c59dbc6eff952e3a2ec40d05440cdfc
danieldenton <danielmdenton@gmail.com>
2023-01-09 14:16:26 -0800

    congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,3 +1,15 @@
-export default function Congrats({ success }) {
-  return <div></div>;
-}
+import React from "react";
+
+export default (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
+};
