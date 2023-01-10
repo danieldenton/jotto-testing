3c7066df18251099dc617a0cb2d8b1f241753762
danieldenton <danielmdenton@gmail.com>
2023-01-09 16:06:08 -0800

    guessed words test

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -22,12 +22,18 @@ test("does not throw warning with expected props", () => {
 });
 
 describe("if there are no words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
   test("renders without error", () => {
-    const wrapper = wrapper({ guessedWords: [] });
     const component = findByTestAttr(wrapper, "component-guessed-words");
     expect(component.length).toBe(1);
   });
-  test("renders instructions to guess a word", () => {});
+  test("renders instructions to guess a word", () => {
+    const instructions = findByTestAttr(wrapper, "guess-instructions");
+    expect(instructions.text().length).not.toBe(0);
+  });
 });
 
 describe("if there are words guessed", () => {});
