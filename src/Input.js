10eb5746783945044cbd166ace5dc275be58e7f1
danieldenton <danielmdenton@gmail.com>
2023-01-09 19:37:14 -0800

    input.js

---
+++ src/Input.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Input() {
+  return (
+    <form data-test="component-input">
+      <input type="input">{}</input>
+      <button>Submit</button>
+    </form>
+  );
+}
